8bf7165d46dffcbc843f8a79576af3353cefa2ee
Ammar Abu Yaman <ammar.abu.yaman@gmail.com>
2025-03-04 19:51:12 +0300

    Add braces tokenization

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,15 @@ fn main() {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
-
+            
             file_contents.chars().for_each(|c| match c {
                 '(' => println!("LEFT_PAREN ( null"),
                 ')' => println!("RIGHT_PAREN ) null"),
+                '{' => println!("LEFT_BRACE {{ null"),
+                '}' => println!("RIGHT_BRACE }} null"),
                 _ => {},
             });
-
-            println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-
+            println!("EOF  null");
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
